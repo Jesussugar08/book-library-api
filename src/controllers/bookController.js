6c81d475109c5e3747f9d1c76ad83f286e2beb00
Jesus Sugar <jesussugar27@gmail.com>
2026-06-08 21:14:51 -0500

    Add getBookById and deleteBook endpoints

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -54,3 +54,63 @@ exports.createBook = async (req, res) => {
       res.status(500).json({ error: error.message })
     }
   }
+
+  exports.getBookById = async (req, res) => {
+    try {
+    
+        const {id} = req.params
+        const userId = req.userId
+
+        const result = await pool.query(
+            `SELECT
+                *
+             FROM books
+
+             WHERE id = $1 AND user_id = $2
+
+            `,
+            [id, userId]
+        )
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                error: 'Does not exist'
+              })
+        }
+        res.status(200).json({
+            success: true,
+            data: result.rows[0]
+          })
+  
+    } catch(error) {
+      res.status(500).json({ error: error.message })
+    }
+  }
+
+  exports.deleteBook = async (req, res) => {
+    try {
+    
+        const {id} = req.params
+        const userId = req.userId
+
+        const result = await pool.query(
+            `DELETE 
+             FROM books
+             WHERE id = $1 and user_id = $2 
+             RETURNING *
+            `,
+            [id, userId]
+        )
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                error: 'Does not exist'
+              })
+        }
+        res.status(200).json({
+            success: true,
+            data: result.rows[0]
+          })
+  
+    } catch(error) {
+      res.status(500).json({ error: error.message })
+    }
+  }
--- src/routes/books.js
+++ src/routes/books.js
@@ -8,5 +8,7 @@ const authMiddleware = require('../middleware/auth')
 
 router.get('/', authMiddleware, controller.getBooks)
 router.post('/', authMiddleware, controller.createBook)
+router.get('/:id', authMiddleware, controller.getBookById )
+router.delete('/:id', authMiddleware, controller.deleteBook)
 
 module.exports = router
