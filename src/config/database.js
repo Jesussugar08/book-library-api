64b8829da35a613fe61a52dc06fd1ba521c3766a
Jesus Sugar <jesussugar27@gmail.com>
2026-06-16 19:20:15 -0500

    refactor for the data base

--- src/config/database.js
+++ src/config/database.js
@@ -1,30 +1,14 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL 
+    ? { rejectUnauthorized: false } 
+    : false
+})
 
-    
+pool.on('connect', () => {
+  console.log('✅ Conectado a PostgreSQL')
 })
 
 module.exports = pool
-
-pool.on('connect', () => {
-
-    console.log('✅ Conectado a PostgreSQL')
-
-})
-
-pool.query('SELECT NOW ()', (err, res) =>{
-
-    if(err){
-        console.log('❌ Error conectandoo a BD', err)
-    }else{
-        console.log('✅ Conectado a postgreSQL', res.rows[0])
-    }
-
-
-})
